98e17ebadb41abf58e99a244b8a847a75780fd2d
Katherine Li <sheungsev@gmail.com>
2024-03-27 15:16:03 +0800

    init

---
+++ index.js
@@ -0,0 +1,58 @@
+const fs = require('fs');
+const path = require('path');
+const ffmpegStatic = require('ffmpeg-static');
+const ffmpeg = require('fluent-ffmpeg')
+
+function unlimitedGifRepetitions(path) {
+  const data = fs.readFileSync(path);
+
+  const index = data.indexOf(Buffer.from([0x21, 0xFF, 0x0B]));
+  if (index < 0) {
+    throw new Error(`Cannot find Gif Application Extension in ${path}`);
+  }
+
+  data[index + 16] = 255;
+  data[index + 17] = 255;
+
+  return data;
+}
+
+function pngToGif(path){
+    try{
+        ffmpeg(path).format('gif')
+        console.log(`png to gif ${file} `);
+    }catch (error){
+        console.error(`Error png to gif ${file} :`, error);
+    }
+}
+
+function batchModifyGifFilesInDirectory(directoryPath) {
+  fs.readdir(directoryPath, (err, files) => {
+    if (err) {
+      console.error('Error reading directory:', err);
+      return;
+    }
+
+    files.forEach(file => {
+      const filePath = path.join(directoryPath, file);
+      const fileExtension = path.extname(file);
+
+      if (fileExtension==='.png'){
+        pngToGif(filePath)
+      }
+
+      if (fileExtension === '.gif') {
+        try {
+          const modifiedData = unlimitedGifRepetitions(filePath);
+          fs.writeFileSync(filePath, modifiedData);
+          console.log(`Modified ${file} `);
+        } catch (error) {
+          console.error(`Error modifying ${file} :`, error);
+        }
+      }
+    });
+  });
+}
+
+const directoryPath = 'C:/Users/ysli/Downloads/pingrayk/55765018';
+batchModifyGifFilesInDirectory(directoryPath);
